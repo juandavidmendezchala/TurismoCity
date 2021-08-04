fffcc4e90fa5bc4e77362762c5c9341e4cd44b4b
matiseisdedos <matiasseisdedosg@gmail.com>
2021-08-03 23:29:19 -0300

    modelo de aeropuertos

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -1,8 +1,8 @@
-const {Sequelize} = require('sequelize');
+const { Sequelize } = require('sequelize');
 const { dbUser, dbPassword, dbHost, dbName } = require('../utils/config/index.js');
 const userModel = require('./user.js')
 const userActivity = require('./activitie.js')
-
+const airportsModel = require('./airports')
 
 const sequelize = new Sequelize(`postgres://${dbUser}:${dbPassword}@${dbHost}/${dbName}`, {
     logging: false,
@@ -11,9 +11,11 @@ const sequelize = new Sequelize(`postgres://${dbUser}:${dbPassword}@${dbHost}/${
 
 const User = userModel(sequelize)
 const Activitie = userModel(sequelize)
+const Airports = airportsModel(sequelize)
 
 module.exports = {
     conn: sequelize,
     User,
-    Activitie
+    Activitie,
+    Airports
 }
