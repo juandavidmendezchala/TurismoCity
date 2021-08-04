61738b322fcb8f9d418f600eb9f549357843c11a
Your Name <you@example.com>
2021-08-04 15:55:09 -0300

    form busqueda con sugerencias

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
-// import reducers from "./reducers";
+import reducers from "./reducers/reducers";
 import thunk from "redux-thunk";
 // comentar esta linea y la 9 si no tienen el redux dev tools instalado
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
 const store = createStore(
-  /*reducers,*/ composeEnhancers(applyMiddleware(thunk))
+  reducers, composeEnhancers(applyMiddleware(thunk))
 );
 
 export default store;
