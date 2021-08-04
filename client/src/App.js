158e57059efb8777134c927809952033a6b79b34
juandavidmendezchala <juandavidmendezchala@gmail.com>
2021-08-04 09:38:39 -0500

    packjson fix

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 import Banner from "./components/Banner/Banner";
 import Home from "./components/Landing/Landing";
 import Navbar from "./components/NavBar/Navbar";
-import FlightCard from "./components/FlightCard/FlightCard";
+//import FlightCard from "./components/FlightCard/FlightCard";
+import Actities from "./components/Activities/Activities";
+
 
 function App() {
   return (
@@ -11,7 +13,7 @@ function App() {
       <Route path="/" component={Banner} />
       {/* /<Route path="/" component={Navbar}></Route> */}
       <Route exact path="/" component={Home}></Route>
-      <Route path="/flights" component={FlightCard}></Route>
+      <Route exact path="/actividades" component={Actities}></Route>   
     </div>
   );
 }
