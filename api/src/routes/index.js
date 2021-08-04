14edd26363a5441953a6d154ab30afa764a9d410
enegraso <sib2000@gmail.com>
2021-08-04 10:55:41 -0300

    ADDED: register and login back

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,11 @@
 const {Router} = require('express');
 
 const flightsRoutes = require('./flights.js');
+const usersRoutes = require('./users')
 
 const router = Router();
 
 router.use('/flights', flightsRoutes)
+router.use(usersRoutes)
 
 module.exports = router;
