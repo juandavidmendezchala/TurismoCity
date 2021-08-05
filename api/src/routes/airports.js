a19992e9d085be3abf909672240dd2b264f2452f
matiseisdedos <matiasseisdedosg@gmail.com>
2021-08-04 22:05:30 -0300

    agregaron algo al package json

--- api/src/routes/airports.js
+++ api/src/routes/airports.js
@@ -2,9 +2,11 @@ const { Router } = require('express');
 const airports = require('../../../airports.json')
 const router = Router();
 
-
+// Esta ruta sirve para tener los datos de los aeropuertos para poder renderizarlos en el front y poder ofrecer sugerencias de aeropuerto, codigo o ciudad.
 router.get('/', (req, res) => {
-    const allAirports = airports
+    const allAirports = airports.filter(e => e.type === "Airports")
+    console.log(allAirports.length)
+    console.log(airports.length)
     res.send(allAirports)
 })
 
