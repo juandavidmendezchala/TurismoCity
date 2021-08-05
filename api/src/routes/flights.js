f77b5b1d2c87cf66f50212ee7cf95bde67729327
Nadia <nadiavinabal@hotmail.com>
2021-08-04 21:26:27 -0300

    backVuelo

--- api/src/routes/flights.js
+++ api/src/routes/flights.js
@@ -1,10 +1,58 @@
 const {Router} = require('express');
+const axios = require('axios')
+const fetch = require('node-fetch');
+
 
 const router = Router();
+const KEY_API = '6100341367b32f6d377e967d'
 
+function getAll (req,res, next) {
+  
+    //legs
+    //departureDateTime -> fecha de salida
+    //arrivalDateTime -> fecha de llegada
+    //departureTime -> hora de salida
+    //arrivalTime -> hora de llegada
+    //duration -> duracion
+    //departureAirportCode -> codigo aeropuesto de salida
+    //arrivalAirportCode -> codigo aeropuesto de llegada
 
-router.get('/', (req, res) => {
-    res.send("hola")
-})
+    //stopoverAirportCodes -> codigo de aeropuesto de llegada
+    //stopoversCount -> escala (cantidad de escala ej 1)
+    //departureTimeMinutes -> hora de salida en minitos 
+    //overnight -> (true/false) -> durante la noche
+    
+    // fares
+    //price.totalAmount
+    const salida = req.params.salida;
+    const llegada = req.params.llegada;
+    const fechaSalida = req.params.fechaSalida;
+    const fechaRegreso = req.params.fechaRegreso;
+    const numAdultos = req.params.numAdultos;
+    const numNinios = req.params.numNinios;
+    const numBebes = req.params.numBebes;
+    const claseCabina = req.params.claseCabina;
+    const moneda = req.params.moneda;
 
-module.exports = router;
+    //solo ida
+    if (!fechaRegreso) {
+        //https://api.flightapi.io/onewaytrip/YOURAPIKEY/LHR/LAX/2019-10-11/2/0/1/Economy/USD
+        axios.get(`https://api.flightapi.io/roundtrip/${KEY_API}/${salida}/${llegada}/${fechaSalida}/${numAdultos}/${numNinios}/${numBebes}/${claseCabina}/${moneda}`)
+        .then(response => {
+  
+            res.send(response.data)
+        }); 
+
+    }
+    
+    axios.get(`https://api.flightapi.io/roundtrip/${KEY_API}/${salida}/${llegada}/${fechaSalida}/${fechaRegreso}/${numAdultos}/${numNinios}/${numBebes}/${claseCabina}/${moneda}`)
+      .then(response => {
+
+          res.send(response.data)
+      }); 
+}
+
+//module.exports = router;
+module.exports = {
+    getAll
+ };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,6 +4,8 @@ const flightsRoutes = require('./flights.js');
 
 const router = Router();
 
-router.use('/flights', flightsRoutes)
+router.use('/flights', flightsRoutes.getAll)
+//const apiRecipe = axios.get(`https://api.flightapi.io/roundtrip/${KEY_API}/${salida}/${llegada}/${fechaSalida}/${fechaRegreso}/${numAdultos}/${numNinios}/${numBebes}/${claseCabina}/${moneda}`)
+
 
 module.exports = router;
