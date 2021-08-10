e72b0040afb55c7c019bb6486846db10070796cc
matiseisdedos <matiasseisdedosg@gmail.com>
2021-08-10 18:48:01 -0300

    ultimos cambios antes de traer back

--- api/src/routes/activities.js
+++ api/src/routes/activities.js
@@ -1,9 +1,94 @@
-const {Router} = require('express');
+const { Router } = require('express');
+const { Activity, User } = require('../models/index')
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send("Activities")
+router.get('/', async (req, res) => {
+    let {
+        filter,
+        order,
+        country,
+        city
+    } = req.body;
+    if (order) {
+        const findBy = city ? {
+            where: {
+                city
+            },
+            order: [
+                [filter, order]
+            ]
+
+        } :
+            {
+                where: {
+                    country
+                },
+                order: [
+                    [filter, order]
+                ]
+
+            }
+        const findByOrder = { order: [[filter, order]] }
+        const getActivities = await Activity.findAll(findBy || findByOrder)
+        return res.send(getActivities)
+    }
+    if (city || country) {
+        const findBy = city ? {
+            where: {
+                city
+            }
+        } :
+            {
+                where: {
+                    country
+                }
+            }
+        const getActivities = await Activity.findAll(findBy)
+        return res.send(getActivities)
+    }
+    const getActivities = await Activity.findAll({})
+    return res.send(getActivities)
+})
+
+router.post('/', async (req, res) => {
+    let {
+        email,
+        name,
+        description,
+        date,
+        price,
+        places,
+        duration,
+        initialTime,
+        comments,
+        passengers,
+        images,
+        country,
+        city
+    } = req.body;
+    const createPack = await Activity.create({
+        name,
+        description,
+        date,
+        price,
+        places,
+        duration,
+        initialTime,
+        comments,
+        passengers,
+        images,
+        country,
+        city,
+        active: true
+    })
+    const findUser = await User.findOne({
+        where: {
+            email
+        }
+    })
+    await findUser.addActivity(createPack)
+    return res.send(createPack)
 })
 
 module.exports = router;
