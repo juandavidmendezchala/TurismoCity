e37b28fd66e84e2f3efd64cc6baa6daa08174241
matiasg1 <mati.gerez7@gmail.com>
2021-08-12 11:32:57 -0300

    Pull

--- api/src/routes/activities.js
+++ api/src/routes/activities.js
@@ -1,5 +1,7 @@
 const {Router} = require('express');
 const {Activity, User} = require('../models/index')
+const { Op } = require("sequelize");
+
 
 const router = Router();
 
@@ -8,7 +10,8 @@ router.get('/', async(req, res) => {
         filter,
         order,
         country,
-        city
+        city,
+        date
     } = req.body;
     if(order) {
         const findBy = city? {
@@ -33,6 +36,34 @@ router.get('/', async(req, res) => {
         const getActivities = await Activity.findAll(findBy || findByOrder)
         return res.send(getActivities)
     }
+    /*const Op = Sequelize.Op;{
+    where: {
+        createdAt: {
+          [Op.between]: ["2018-07-08T14:06:48.000Z", "2019-10-08T22:33:54.000Z"]
+        }
+      }}*/
+     /* if (date.desde != '') {
+      const findByDate = date? {
+        query.push({
+
+            procesador_fecha: { 
+              [Op.between]: [date.desde + ' 00:00', date.hasta + ' 23:59'],
+            },
+
+        })
+    }:*/
+    const dbresp = await Table.findAll({
+        attributes: [date],
+        where: {
+          createdAt: {
+             [Op.between]: [startDate, endDate],
+          },
+        },
+        logging: console.log,
+        raw: true,
+        order: [['createdAt', 'ASC']],
+        // limit: count,
+      });
     const findBy = city? {
         where: {
             city
