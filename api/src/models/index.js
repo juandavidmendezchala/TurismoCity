5c1359aac5c28ab173928e5bef9db8dff4f139f2
Federico Ortiz <sib2000@gmail.com>
2021-08-17 11:29:59 -0300

    Update index.js

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -4,10 +4,40 @@ const userModel = require('./user.js')
 const activityModel = require('./activity.js')
 const airportsModel = require('./airports')
 
-const sequelize = new Sequelize(`postgres://${dbUser}:${dbPassword}@${dbHost}/${dbName}`, {
-    logging: false,
-    native: false
-})
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+let sequelize =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize({
+        database: DB_NAME,
+        dialect: "postgres",
+        host: DB_HOST,
+        port: 5432,
+        username: DB_USER,
+        password: DB_PASSWORD,
+        pool: {
+          max: 3,
+          min: 1,
+          idle: 10000,
+        },
+        dialectOptions: {
+          ssl: {
+            require: true,
+            // Ref.: https://github.com/brianc/node-postgres/issues/2009
+            rejectUnauthorized: false,
+          },
+          keepAlive: true,
+        },
+        ssl: true,
+      })
+    : new Sequelize(
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+        { logging: false, native: false }
+      );
+
+// const sequelize = new Sequelize(`postgres://${dbUser}:${dbPassword}@${dbHost}/${dbName}`, {
+//  logging: false,
+//    native: false
+// })
 
 const User = userModel(sequelize)
 const Activity = activityModel(sequelize)
