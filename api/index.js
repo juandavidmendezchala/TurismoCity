88cf565d7dd224fa51b03f6565996c8d96866bd6
Federico Ortiz <sib2000@gmail.com>
2021-08-17 22:34:50 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors"); // para poder hacer peticiones desde cualquier punto (tambien se puede configurar de donde recibir las peticiones)
+// const cors = require("cors"); // para poder hacer peticiones desde cualquier punto (tambien se puede configurar de donde recibir las peticiones)
 const { conn } = require("./src/models/index.js");
 const routes = require("./src/routes/index");
 
@@ -9,15 +9,13 @@ const { PORT } = require("./src/utils/config/index.js");
 const errorHandler = require("./src/utils/middelwares/errorHandler.js");
 const setHeaders = require("./src/utils/middelwares/setHeaders.js");
 
-app.use(cors()); // uso de cors definido anteriormente
+// app.use(cors()); // uso de cors definido anteriormente
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json({ limit: "50mb" }));
 app.use(morgan("dev"));
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000,https://turismo-city.vercel.app'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 app.use(errorHandler);
