21df26f51d8700ccd9422a2ee4e665019bf4b918
Federico Ortiz <sib2000@gmail.com>
2021-08-17 22:16:18 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -13,6 +13,13 @@ app.use(cors()); // uso de cors definido anteriormente
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json({ limit: "50mb" }));
 app.use(morgan("dev"));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 app.use(errorHandler);
 app.use(setHeaders);
 
