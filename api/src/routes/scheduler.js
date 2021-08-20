48f81595875e00e5a5be200527d1b2e93ce829be
enegraso <sib2000@gmail.com>
2021-08-20 15:30:28 -0300

    ADDED: back full scheduler

--- api/src/routes/scheduler.js
+++ api/src/routes/scheduler.js
@@ -36,4 +36,38 @@ router.post("/scheduler", async (req, res) => {
   }
 });
 
+router.put("/scheduler", async (req, res) => {
+    try {
+      let { idSched, fechaini, fecharec, tiempo, notas } = req.body;
+      let objGuardar = {
+        fechaini,
+        fecharec,
+        tiempo,
+        notas,
+      };
+      let modificado = await Scheduler.update(objGuardar, { where: { id: idSched }});
+      return res.send(modificado);
+    } catch (err) {
+      return res.send({
+        message: "No se pudo guardar la agenda" + err,
+      });
+    }
+  });
+
+router.delete("/scheduler/:idSched", async (req,res) => {
+    try {
+        let { idSched } = req.params
+        const eliminado = Scheduler.destroy({
+            where: {
+                id: idSched
+            }
+        })
+        return res.send(eliminado)
+    } catch (err) {
+        return res.send({
+          message: "No se pudo guardar la agenda" + err,
+        });
+      }
+})
+
 module.exports = router;
