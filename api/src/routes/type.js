922c79835f01112dc316ac1caf903099a97ad8ec
juandavidmendezchala <juandavidmendezchala@gmail.com>
2021-08-23 20:04:50 -0500

    hecho type

--- api/src/routes/type.js
+++ api/src/routes/type.js
@@ -5,8 +5,15 @@ const data = require('./typeData')
 const router = Router();
 
 router.get('/', async(req, res) => {
-    const createTypes = await Type.create(data.types)
-    return res.send(createTypes)
+    const types = await Type.findAll({})
+    if(types[0]) {
+        return res.send(types)
+    } else {
+        await data.types.forEach(type => {
+            Type.create(type)        
+        });
+    }
+    return res.send("Created")
 })
 
 module.exports = router;
