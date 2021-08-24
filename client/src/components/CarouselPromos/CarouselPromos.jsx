9716b4171ad2a29ccdf0cdbb1c271a4b8339091e
Your Name <janoncarlos@gamil.com>
2021-08-24 11:23:10 -0300

    esta listo el componente carousel adminpost falta unirlo al back y la actions que hizo David

--- client/src/components/CarouselPromos/CarouselPromos.jsx
+++ client/src/components/CarouselPromos/CarouselPromos.jsx
@@ -26,14 +26,14 @@ const CarouselPromos = () => {
     const NextArrow = ({ onClick }) => {
         return (
             <div className='arrow nextP' onClick={onClick}>
-                🔹
+                🔶
             </div>
         )
     }
     const PrevArrow = ({ onClick }) => {
         return (
             <div className='arrow prevP' onClick={onClick}>
-                🔹
+                🔶
             </div>
         )
     }
