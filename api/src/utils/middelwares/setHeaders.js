490925c00e923facea20ddf6b6c54b6cef6f8c17
juandavidmendezchala <juandavidmendezchala@gmail.com>
2021-08-26 00:11:40 -0500

    new fixxx

--- api/src/utils/middelwares/setHeaders.js
+++ api/src/utils/middelwares/setHeaders.js
@@ -1,5 +1,5 @@
-function setHeaders (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://lowhenry-psi.vercel.app')
+/* function setHeaders (req, res, next) {
+    res.header('Access-Control-Allow-Origin', value:'true')
     res.header('Access-Control-Allow-Credentials', true)
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type')
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
@@ -7,3 +7,21 @@ function setHeaders (req, res, next) {
 }   
 
 module.exports = setHeaders;
+ */
+
+module.exports = {
+    async setHeaders() {
+      return [
+        {
+          // matching all API routes
+          source: "/api/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        }
+      ]
+    }
+  };
