893965706b5f17b12803d3a2a862573ffcdb6fbc
enegraso <sib2000@gmail.com>
2021-08-26 14:47:15 -0300

    MAILSENDER: sirve para avisar agendas y deseos

--- api/src/routes/sendemails.js
+++ api/src/routes/sendemails.js
@@ -1,93 +1,295 @@
-const { Rueter, Router } = require('express')
-const { Scheduler, User, MailSends } = require('../models/index')
-const nodemailer = require('nodemailer')
-const router = Router()
+const { Rueter, Router } = require("express");
+const {
+  Scheduler,
+  User,
+  MailSends,
+  Whishes,
+  Activity,
+} = require("../models/index");
+const nodemailer = require("nodemailer");
+const { Op } = require("sequelize");
+const router = Router();
 
 contentHTML = `
 <h2>Mensaje de Agenda de lowHenry</h2>
-`
+`;
+
+contentHTML2 = `
+<h2>Mensaje de Deseos Guardados en lowHenry</h2>
+`;
+
 const transporter = nodemailer.createTransport({
-    host: 'mail.sib-2000.com.ar',
-    port: 587,
-    secure: false,
-    auth: {
-        user: 'lowhenry@sib-2000.com.ar',
-        pass: 'lowhenry2021'
+  host: "mail.sib-2000.com.ar",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "lowhenry@sib-2000.com.ar",
+    pass: "lowhenry2021",
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+var activitiesAEnviar = [];
+
+router.post("/sched-email/:fecha", async (req, res) => {
+  const { fecha } = req.params;
+  console.log(fecha);
+  const listEmail = await Scheduler.findAll({
+    include: User,
+    where: {
+      fecharec: fecha,
+      enviado: false,
     },
-    tls: {
-        rejectUnauthorized: false
-    }
-})
+  });
+  let emailsenviados = 0;
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let fechactual = day + "/" + month.toString().padStart(2, "0") + "/" + year;
+  let horactual = hour + ":" + minute;
 
-router.post('/sched-email/:fecha', async (req, res) => {
-    const { fecha } = req.params 
-    console.log(fecha)
-    const listEmail = await Scheduler.findAll({
-        include: User,
-        where: {
-            fecharec: fecha,
-            enviado: false
-        }
-    })
-    let emailsenviados = 0
-    let date = new Date()
-    let day = date.getDate()
-    let month = date.getMonth() + 1
-    let year = date.getFullYear()
-    let hour = date.getHours()
-    let minute = date.getMinutes()
-    let fechactual = day+"/"+month.toString().padStart(2,'0')+"/"+year
-    let horactual = hour+":"+minute
-
-    const aenviarEmail = listEmail.map(correo => {
-        console.log(correo.user.email)
-        emailsenviados++
-        enviandoEmail(correo.user.email, correo.notas, correo.user.name)
-        guardandoEnvio(fecha,horactual,correo.user.email)
-        setearEnviado(correo.id)
-        // console.log(listEmail[0]["id"])
-
- 
-    })
-    if (emailsenviados === 0)
-    return res.status(200).json("No se encontraron recordatorios")
-    else
-    return res.status(200).json("Se enviaron " + emailsenviados + " recordatorios")
-})
+  const aenviarEmail = listEmail.map((correo) => {
+    console.log(correo.user.email);
+    emailsenviados++;
+    enviandoEmail(correo.user.email, correo.notas, correo.user.name);
+    guardandoEnvio(fecha, horactual, correo.user.email);
+    setearEnviado(correo.id);
+    // console.log(listEmail[0]["id"])
+  });
+  if (emailsenviados === 0)
+    return res.status(200).json("No se encontraron recordatorios");
+  else
+    return res
+      .status(200)
+      .json("Se enviaron " + emailsenviados + " recordatorios");
+});
 
 async function enviandoEmail(correo, texto, name) {
-    const info = await transporter.sendMail({
-        from: 'Low Henry te recuerda!, <lowhenry@sib-2000.com.ar>',
-        to: correo,
-        subject: "Agendaste en nuestro servicio el siguiente recordatorio",
-        html: "<h1>Hola " + name + "</h1><br>" + contentHTML + "<br><h2>Agendaste</h2><br><h3>" + texto + "<h3><br><h3>Muchas Gracias por elegir nuestros servicios<h3><br><img src='' alt='LowHenry' />"
-    })
-} 
+  const info = await transporter.sendMail({
+    from: "Low Henry te recuerda!, <lowhenry@sib-2000.com.ar>",
+    to: correo,
+    subject: "Agendaste en nuestro servicio el siguiente recordatorio",
+    html:
+      "<h1>Hola " +
+      name +
+      "</h1><br>" +
+      contentHTML +
+      "<br><h2>Agendaste</h2><br><h3>" +
+      texto +
+      "<h3><br><h3>Muchas Gracias por elegir nuestros servicios<h3><br><img src='' alt='LowHenry' />",
+  });
+}
 
 const guardandoEnvio = async (date, time, emailsend) => {
-    objGuardar = {
-        date,
-        time,
-        emailsend
-    }
-    const guardaenvio = await MailSends.create(objGuardar)
-}
+  objGuardar = {
+    date,
+    time,
+    emailsend,
+  };
+  const guardaenvio = await MailSends.create(objGuardar);
+};
 
 const setearEnviado = async (id) => {
-    objGuardar = {
-        enviado: true,
+  objGuardar = {
+    enviado: true,
+  };
+  let modificado = await Scheduler.update(objGuardar, { where: { id: id } });
+};
+
+router.get("/emailsended", async (req, res) => {
+  try {
+    const mailsended = await MailSends.findAll();
+    console.log(mailsended);
+    res.status(200).send(mailsended);
+  } catch (err) {
+    res.status(400).send("No se pudo obtener los envios" + err);
+  }
+});
+
+var actividades = [];
+let deseosenviados = 0;
+
+router.get("/whish-emails", async (req, res) => {
+  const WhishList = await Whishes.findAll({
+    include: User,
+  });
+
+  WhishList &&
+    WhishList.map( async (whish) => {
+      enviarWhishDestino(
+        whish.destino,
+        whish.fechaini,
+        whish.fechafin,
+        whish.presupuesto,
+        whish.cupos,
+        whish.user.email,
+        whish.user.name
+      );
+    });
+
+  if ((deseosenviados = 0))
+    return res.status(200).send("No se encontraron listas de deseos");
+  else {
+    return res.status(200).json({
+      message: "Se enviaron emails coincidentes con deseos guardados",
+      enviados: deseosenviados,
+    });
+  }
+});
+
+const enviarWhishDestino = async (
+  destino,
+  fechini,
+  fechfin,
+  presup,
+  cupos,
+  email,
+  nombre
+) => {
+  //ver si puedo traer actividades coincidentes con la agenda
+  var arraycondestino = [];
+  if (!destino || destino === "") {
+    try {
+      const getAllActivity = await Activity.findAll({
+        include: User,
+        where: {
+          date: {
+            [Op.between]: [fechini, fechfin],
+          },
+          price: {
+            [Op.between]: [0, presup],
+          },
+          places: {
+            [Op.gte]: [cupos],
+          },
+        },
+      });
+      getAllActivity &&
+        getAllActivity.map((activity) => {
+          objectActivity = {
+            idactivity: activity.id,
+            dia: activity.date,
+            destino: activity.city,
+            precio: activity.price,
+            nombre: activity.name,
+            detalle: activity.description,
+            cupos: activity.places,
+            ofrece: activity.user.email,
+          };
+          arraycondestino.push(objectActivity);
+        });
+      if (arraycondestino.length > 0) {
+        var linea1 =
+          "Se encontraron las siguientes actividades correspondientes a tu deseo<br <br/>";
+        var texto2 = "";
+        arraycondestino.map((acti) => {
+            let link = "https://lowhenry-psi.vercel.app/activity/" + acti.idactivity
+            texto2 += "<a href=" + link +">" + acti.nombre + "</a><br/>";
+          });
+        var texto = linea1 + texto2;
+        deseosenviados++;
+        // console.log(deseosenviados)
+        // console.log(texto)
+        // Mandar email de a cuerdo al deseo y sus actividades
+        enviandoEmailWhish(email, texto, nombre);
+        texto = "";
+        linea1 = "";
+        texto2 = "";
+      }
+      return arraycondestino;
+    } catch (err) {
+      console.log(err);
+
+      // return res.send({ message: "No se pudo obtener la agenda" + err, });
     }
-    let modificado = await Scheduler.update(objGuardar, { where: { id: id }})
+  } else {
+    try {
+      // const { destino, fechini, fechfin, presup, cupos } = req.params;
+      // const destabuscar = destino.toUpperCase()
+      const getAllActivity = await Activity.findAll({
+        include: User,
+        where: {
+          city: {
+            [Op.iLike]: "%" + [destino] + "%",
+          },
+          date: {
+            [Op.between]: [fechini, fechfin],
+          },
+          price: {
+            [Op.between]: [0, presup],
+          },
+          places: {
+            [Op.gte]: [cupos],
+          },
+        },
+      });
+
+      getAllActivity &&
+        getAllActivity.map((activity) => {
+          objectActivity = {
+            idactivity: activity.id,
+            dia: activity.date,
+            destino: activity.city,
+            precio: activity.price,
+            nombre: activity.name,
+            detalle: activity.description,
+            cupos: activity.places,
+            ofrece: activity.user.email,
+          };
+          arraycondestino.push(objectActivity);
+        });
+
+      if (arraycondestino.length > 0) {
+        var linea1 =
+          "Se encontraron las siguientes actividades correspondientes a tu deseo<br <br/>";
+        var texto2 = "";
+        arraycondestino.map((acti) => {
+           let link = "https://lowhenry-psi.vercel.app/activity/" + acti.idactivity
+          texto2 += "<a href=" + link +">" + acti.nombre + "</a><br/>";
+        });
+        var texto = linea1 + texto2;
+        deseosenviados++;
+        // console.log(deseosenviados)
+        // console.log(texto)
+        // Mandar email de a cuerdo al deseo y sus actividades
+        enviandoEmailWhish(email, texto, nombre);
+        texto = "";
+        linea1 = "";
+        texto2 = "";
+      }
+      return arraycondestino;
+      // return res.statusend(getAllActivity);
+    } catch (err) {
+      console.log(err);
+      // return res.send({
+      //  message: "No se pudo obtener la agenda" + err,
+      // });
+    }
+  }
+  
+};
+
+
+async function enviandoEmailWhish(correo, texto, name) {
+  console.log("enviando cporre", correo, texto, name);
+   const info = await transporter.sendMail({
+    from: "Low Henry encontró actividades para vos!, <lowhenry@sib-2000.com.ar>",
+    to: correo,
+    subject: "Segun tus deseos guardados, aquí te mostramos actividades",
+    html:
+      "<h1>Hola " +
+      name +
+      "</h1><br>" +
+      contentHTML +
+      "<br><h2>Deseo de actividades</h2><br><h3>" +
+      texto +
+      "<h3><br><h3>Muchas Gracias por elegir nuestros servicios<h3><br><img src='' alt='LowHenry' />",
+  });
 }
 
-router.get("/emailsended", async (req,res) => {
-    try {
-        const mailsended = await MailSends.findAll()
-        console.log(mailsended)
-        res.status(200).send(mailsended)
-    } catch (err) {
-        res.status(400).send("No se pudo obtener los envios"+err)
-    }
-})
-
-module.exports = router
+module.exports = router;
--- client/src/components/EmailSender/EmailSender.jsx
+++ client/src/components/EmailSender/EmailSender.jsx
@@ -1,40 +1,42 @@
-import React, { useEffect, useState  } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { REACT_APP_API } from "../../store/Consts/Consts";
 import style from "../../styles/Emailsend.module.css";
-import axios from 'axios'
+import axios from "axios";
 
 const EmailSender = (props) => {
-  
-  let date = new Date()
-  let day = date.getDate()
-  let month = date.getMonth() + 1
-  let year = date.getFullYear()
-  let fechactual = day+"-"+month.toString().padStart(2,'0')+"-"+year
-  console.log(fechactual)
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  let fechactual = day + "-" + month.toString().padStart(2, "0") + "-" + year;
+  console.log(fechactual);
 
-  console.log(fechactual)
+  console.log(fechactual);
   const [input, setInput] = useState({
     fecha: fechactual,
   });
   const [result, setResult] = useState([{}]);
-  const [resultado, setResultado] = useState({})
+  const [resultado, setResultado] = useState({});
+  const [deseados, setDeseados] = useState({});
 
   const userSingin = useSelector((state) => state.userSignin);
   const { userInfo } = userSingin;
 
-  useEffect( async () => {
-    const mailsenviados = await axios.get(`${REACT_APP_API}/emailsended`)
-    setResultado(mailsenviados)
-    console.log(mailsenviados.data)
-  },[result])
+  useEffect(async () => {
+    const mailsenviados = await axios.get(`${REACT_APP_API}/emailsended`);
+    setResultado(mailsenviados);
+    console.log(mailsenviados.data);
+  }, [result]);
 
   const handleEnviar = async (e) => {
     e.preventDefault();
-     const enviando = await axios.post(`${REACT_APP_API}/sched-email/${input.fecha}`)
-     /* .then(response => setResult(response)) */
-     setResult(enviando)
-    console.log(enviando)
+    const enviando = await axios.post(
+      `${REACT_APP_API}/sched-email/${input.fecha}`
+    );
+    /* .then(response => setResult(response)) */
+    setResult(enviando);
+    console.log(enviando);
   };
 
   function handleChange(e) {
@@ -44,32 +46,60 @@ const EmailSender = (props) => {
     });
   }
 
+  async function handleDesear(e) {
+    e.preventDefault();
+    const deseando = await axios.get(`${REACT_APP_API}/whish-emails`);
+    setDeseados(deseando);
+    console.log(deseando.data)
+  }
+
   if (userInfo) {
-  return (
-    <>
-
-    <h1 className={style.tituno}>Enviar los recordatorios (por ahora a mano)</h1>
-    <div className={style.boxform}>
-      <form>
-        <label>Fecha de recordatorio</label>
-        <input
-          id="fecha"
-          name="fecha"
-          type="date"
-          value={input.fecha}
-          onChange={handleChange}
-        ></input>
-        <button onClick={handleEnviar}> Enviar Recordatorios</button>
-      </form>
-      </div>
+    return (
+      <>
+        <h1 className={style.tituno}>
+          Enviar los recordatorios (por ahora a mano)
+        </h1>
+        <div className={style.boxform}>
+          <form>
+            <label>Fecha de recordatorio</label>
+            <input
+              id="fecha"
+              name="fecha"
+              type="date"
+              value={input.fecha}
+              onChange={handleChange}
+            ></input>
+            <button onClick={handleEnviar}> Enviar Recordatorios</button>
+          </form>
+        </div>
         <h1 className={style.tituno}>{result.data}</h1>
-        <h2 className={style.titdos}>Recordatorios enviados: { !resultado.data ? "0" : resultado.data.length }</h2> 
-    </>
-  );}
+        <h2 className={style.titdos}>
+          Recordatorios enviados:{" "}
+          {!resultado.data ? "0" : resultado.data.length}
+        </h2>
+        <hr />
+        <h1 className={style.tituno}>
+          Enviar actividades a usuarios que guardaron deseos
+        </h1>
+
+        <div className={style.boxform}>
+          <form>
+            <label>Avisar actividades a quienes haya guardado sus deseos</label>
+            <button onClick={handleDesear}>
+              Enviar Enviar mail de actividades
+            </button>
+          </form>
+        </div>
+        <h1 className={style.tituno}>Deseos {!deseados ? "" : deseados.data.message}</h1>
+              </>
+    );
+  }
 
   return (
     <>
-      <h1 className={style.tituno}>Debe estar logueado para como Admin para enviar correos</h1>
+      <h1 className={style.tituno}>
+        Debe estar logueado para como Admin para enviar correos
+      </h1>
     </>
   );
 };
