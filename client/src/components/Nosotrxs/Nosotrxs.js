0cba411ee2759ca65fb87f7adb172fa5fe325326
Agus <sebastianlifchitz@Macbook-de-Agus.local>
2021-08-26 13:49:32 -0300

    Cambios Newsletter y Nosotros

--- client/src/components/Nosotrxs/Nosotrxs.js
+++ client/src/components/Nosotrxs/Nosotrxs.js
@@ -10,7 +10,7 @@ const Nosotrxs = () => {
             <div className="FirstBlock">
                 <img className="FotoQuienesSomos" src={foto} alt="Foto representativa"></img>
                 <h3 className="QuienesSomos">Somos la empresa de viajes que te pone en primer lugar</h3>
-                <img className="FotoAvioncito" src="https://www.pikpng.com/pngl/b/84-845652_dibujo-avion-de-papel-clipart.png"></img>
+                {/* <img className="FotoAvioncito" src="https://www.pikpng.com/pngl/b/84-845652_dibujo-avion-de-papel-clipart.png"></img> */}
             </div>
             <div className="SecondBlock">
                 <img className="SecondBlockImg" src={block}></img>
