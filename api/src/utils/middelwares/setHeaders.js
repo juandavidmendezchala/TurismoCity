f695c3f9319aaa6aa501dc7f7901b900e1089e6b
juandavidmendezchala <juandavidmendezchala@gmail.com>
2021-08-25 23:52:28 -0500

    new fixxx

--- api/src/utils/middelwares/setHeaders.js
+++ api/src/utils/middelwares/setHeaders.js
@@ -1,5 +1,5 @@
 function setHeaders (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Origin', 'https://api.flightapi.io/')
     res.header('Access-Control-Allow-Credentials', true)
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type')
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
