4f7d40943f91c9503caa4ba905380fead4390e57
Flor Lomenzo <mflomenzo@gmail.com>
2021-08-27 15:53:59 -0300

    Update getFlights.js

--- client/src/store/actions/getFlights.js
+++ client/src/store/actions/getFlights.js
@@ -12,7 +12,7 @@ const apiKey12 = "61198e61747a9a053255ffdf"
 const apiKey8 = "612702f5747a9a05325621c5" 
 const apiKey9 = "61270317747a9a05325621c6" 
 const apiKey10 = "61270265747a9a05325621c3"
-const apiKey7 = "61270220747a9a05325621c2"
+const apiKey7 = "61293435747a9a05325624f4"
 const apiKey13 = "6126c2aa747a9a0532561ea2"
 
 export function getFlights(p, index = 1) {
