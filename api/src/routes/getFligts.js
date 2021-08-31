64d9c96840ff61b320b3696eb904d7297a712a6d
juandavidmendezchala <juandavidmendezchala@gmail.com>
2021-08-31 14:43:38 -0500

    fix vuelo

--- api/src/routes/getFligts.js
+++ api/src/routes/getFligts.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const {Router} = require('express');
 const apiKey2 = "612716d4747a9a05325621c9"
+const apiKey3 = "61270317747a9a05325621c6"
 
 const router = Router();
 
@@ -15,7 +16,7 @@ router.get('/:fromPlace/:toPlace/:fromDate/:adults/:kids/:babies/:classFlight/:c
         classFlight,
         currency
     } = req.params
-    const url = `https://api.flightapi.io/onewaytrip/${apiKey2}/${fromPlace}/${toPlace}/${fromDate}/${adults}/${kids}/${babies}/${classFlight}/${currency}`
+    const url = `https://api.flightapi.io/onewaytrip/${apiKey3}/${fromPlace}/${toPlace}/${fromDate}/${adults}/${kids}/${babies}/${classFlight}/${currency}`
     const {data} = await axios.get(url)
     return res.send(data)
 })
